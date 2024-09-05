79279226384750fe4a52a0d0cd6720fffedd3e1e
Anel <akhmetova97@gmail.com>
2024-09-05 16:40:43 +0500

    modalByTime

--- dist/script.js
+++ dist/script.js
@@ -258,8 +258,16 @@ const modals = () => {
   }
   function showModalByTime(selector, time) {
     setTimeout(function () {
-      document.querySelector(selector).style.display = 'block';
-      document.body.style.overflow = "hidden";
+      let display;
+      document.querySelectorAll('[data-modal]').forEach(item => {
+        if (getComputedStyle(item).display !== 'none') {
+          display = 'block';
+        }
+      });
+      if (!display) {
+        document.querySelector(selector).style.display = 'block';
+        document.body.style.overflow = "hidden";
+      }
     }, time);
   }
   function calcScroll() {
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -70,8 +70,18 @@ const modals = () => {
 
     function showModalByTime(selector, time) {
         setTimeout(function() {
-            document.querySelector(selector).style.display = 'block';
-            document.body.style.overflow = "hidden";
+            let display;
+
+            document.querySelectorAll('[data-modal]').forEach(item => {
+                if (getComputedStyle(item).display !== 'none') {
+                    display = 'block';
+                }
+            });
+
+            if (!display) {
+                document.querySelector(selector).style.display = 'block';
+                document.body.style.overflow = "hidden";
+            }
         }, time);
     }
 
